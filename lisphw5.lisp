61069f76aa1975c9e4a7c5f8b30635db28a34170
Chase Pavlu <chase.pavlu@gmail.com>
2016-03-29 23:44:14 -0500

    Update lisphw5.lisp

--- lisphw5.lisp
+++ lisphw5.lisp
@@ -2,3 +2,23 @@
 ;Chase Pavlu
 ;Homework 5
 
+
+(defun flatten (li)
+  (cond ((null li) nil)
+        ((atom li) `(,li) ) 
+        (t (mapcan #'flatten li))))
+
+(princ "(a (a b) (a b (a c)))))
+
+")
+(princ (flatten `(a (a b) (a b (a c)))))
+(defun repeat()
+(princ "Would you like to repeat? (1 if yes)
+")
+(setq repeat (read))
+(if (= repeat 1)
+(princ "Sorry")
+(princ "Goodbye")
+)
+)
+(repeat)
